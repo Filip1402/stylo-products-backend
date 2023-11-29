7f4a49d0cea97a7b4286570e731f2b4cce7b6daa
Josip Budak <jbudak20@student.foi.hr>
2023-11-29 12:23:06 +0100

    Created endpoint that retrieves all products

--- src/services/ProductsService.js
+++ src/services/ProductsService.js
@@ -1,21 +1,18 @@
 const axios = require("axios");
+const commerceToolsApi = require("../utils/CommerceToolsApiClient");
 
-const URL_GET_PRODUCTS = `${process.env.CONTENTFUL_API_BASE_URL}/spaces/${process.env.CONTENTFUL_API_SPACEID}/entries`;
-const product_content_type = "commercetoolsProduct";
+const URL_GET_PRODUCTS = `${commerceToolsApi.apiURLBase}/${commerceToolsApi.projectKey}/products`;
 
 async function getAllProducts() {
   try {
-    const token = process.env.CONTENTFUL_DELIVERY_API_ACCESS_TOKEN;
+    const bearerToken = await commerceToolsApi.getAccessToken();
     const response = await axios.get(URL_GET_PRODUCTS, {
       headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      params: {
-        content_type: product_content_type,
+        Authorization: `Bearer ${bearerToken}`,
       },
     });
 
-    return response.data.items;
+    return response.data.results || [];
   } catch (err) {
     console.error(err);
   }
