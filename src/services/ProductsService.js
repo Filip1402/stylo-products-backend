7c0d386e3492a0eaca0fed1a46c75fe5a01f6dbc
robert4361 <robert4361@gmail.com>
2023-12-01 10:00:17 +0100

    Add function for getting homepage products by SKU

--- src/services/ProductsService.js
+++ src/services/ProductsService.js
@@ -1,6 +1,9 @@
 const axios = require("axios");
+const productsAPI = require("../utils/CommerceToolsApiClient");
+const categoriesAPI = require("../utils/CommerceToolsApiClient");
 
 const URL_GET_PRODUCTS = `${process.env.CONTENTFUL_API_BASE_URL}/spaces/${process.env.CONTENTFUL_API_SPACEID}/entries`;
+const URL_GET_PRODUCT = `${productsAPI.apiURLBase}/${productsAPI.projectKey}/product-projections/search`;
 const product_content_type = "commercetoolsProduct";
 
 async function getAllProducts() {
@@ -21,6 +24,27 @@ async function getAllProducts() {
   }
 }
 
+async function getProductForHomepage(id) {
+  try {
+    const bearerToken = await productsAPI.getAccessToken();
+    const response = await axios.get(URL_GET_PRODUCT + `?filter=variants.sku:"${id}"`, {
+      headers: {
+        Authorization: `Bearer ${bearerToken}`,
+      },
+    });
+    let shoe = response.data.results[0];
+    return {
+      name: shoe.name["en-US"],
+      available: shoe.masterVariant.availability.isOnStock,
+      price: shoe.masterVariant.prices[0].value.centAmount / 100,
+      image: shoe.masterVariant.images[0].url
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
 module.exports = {
   getAllProducts,
+  getProductForHomepage
 };
